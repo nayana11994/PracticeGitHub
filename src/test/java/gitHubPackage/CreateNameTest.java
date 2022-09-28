95fb79cca6315f65d22c7ef490878c004ead633a
admin <admin@DESKTOP-MSBUJAU>
2022-09-28 15:40:06 +0530

    Name is created

---
+++ src/test/java/gitHubPackage/CreateNameTest.java
@@ -0,0 +1,13 @@
+package gitHubPackage;
+
+import org.testng.annotations.Test;
+
+public class CreateNameTest 
+{
+	@Test
+	public void create()
+	{
+		System.out.println("---Name is created---");
+	}
+
+}
